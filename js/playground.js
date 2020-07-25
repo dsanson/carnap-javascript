6a7b6d12c870741604b12f3e4d4e68497d6e0013
David Sanson <dsanson@gmail.com>
2020-07-25 12:52:02 -0500

    first try at a working checklist

--- js/playground.js
+++ js/playground.js
@@ -1,5 +1,34 @@
-// proof of concept
+function checklist() {
+    try {
+        var as = JSON.parse(AssignmentState);
+    }
+    catch {
+        var as = {}
+        as["syllabus"] = true
+    }
+
+    $(".task-list :checkbox").each(function() {
+        $( this ).prop("disabled", false);
+        if (as[$( this ).val()]) {
+            $( this ).prop("checked", true);
+        }
+        $( this ).click(function () {
+            if ($( this ).is(":checked")) {
+                as[$( this ).val()] = true;
+            }
+            else {
+                as[$( this ).val()] = false;
+            }
+            console.log(as);
+            putAssignmentState(JSON.stringify(a));
+        });
+    }); 
+}
 
 $( document ).ready(function() {
-    $(".task-list").prop("disabled", false);
-)}
+    checklist();     
+})
+
+
+
+
