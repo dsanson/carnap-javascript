02db6696740b8a8596d7f850df21dfa48507f419
David Sanson <dsanson@gmail.com>
2020-07-25 14:58:44 -0500

    adding comments to script

--- js/checklist.js
+++ js/checklist.js
@@ -1,4 +1,7 @@
 function checklist() {
+    // if we can't fetch AssignmentState, that means we are not
+    // a Carnap assignment. So we'll just go ahead and create an
+    // empty object.
     try {
         var as = JSON.parse(AssignmentState);
     }
@@ -6,20 +9,27 @@ function checklist() {
         console.log("Unable to parse AssignmentState");
         var as = {}
     }
-
+    
+    // if this is the first time we run on a given assignment page,
+    // we need to create as["Checklist Items"]
     if (typeof as["Checklist Items"] === "undefined") {
         as["Checklist Items"] = {} 
     }
 
     $(".task-list :checkbox").each(function() {
+        // pandoc disables all checkboxes by default, so we undisable them
         $( this ).prop("disabled", false);
+
+        // apply stored settings from AssignmentState
         if (as["Checklist Items"][$( this ).val()]) {
             $( this ).prop("checked", true);
         }
         else {
             $( this ).prop("checked", false);
         }
+
         $( this ).click(function () {
+            // when an input box is clicked, update AssignmentState
             if ($( this ).is(":checked")) {
                 as["Checklist Items"][$( this ).val()] = true;
             }
@@ -27,7 +37,15 @@ function checklist() {
                 as["Checklist Items"][$( this ).val()] = false;
             }
             console.log(as);
-            putAssignmentState(JSON.stringify(as));
+            
+            // If we can't putArgumentState, that probably means we aren't
+            // a Carnap.io assignment.
+            try {
+                putAssignmentState(JSON.stringify(as));
+            }
+            catch {
+                console.log("Unable to putArgumentState")
+            }
         });
     }); 
 }
