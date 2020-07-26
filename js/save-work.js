b659b174adfd1d30ce5f9bb237d4b371ebdb1ef9
David Sanson <dsanson@gmail.com>
2020-07-26 16:20:49 -0500

    still working on save-work

--- js/save-work.js
+++ js/save-work.js
@@ -12,6 +12,8 @@ function initSaveWork() {
     as = JSON.parse(AssignmentState);
   } catch {
     console.log('Unable to parse AssignmentState');
+    const dummyAssignmentState = '{"Saved Work":{"saveAs:1.3":"Bob","saveAs:1.7":"Badly","saveAs:1.5":"Show (Q → P)\\nP :PR","saveAs:1.4":["-","-","-","F","-","F","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","T","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-","-"]}}';
+    as = JSON.parse(dummyAssignmentState);
   }
 
   // If this is the first time we run on a given assignment page,
@@ -21,24 +23,36 @@ function initSaveWork() {
   }
 
   function saveWork() {
-    // Syntax Checking
-    // Translation
-    $('[data-carnap-type=translate]').each(function () {
-      // Use data-carnap-submission as unique identifier
-      // This will break more than one problem with same label on page
+    // Syntax Checking (not implemented)
+    // Translation, and Qualitative Short Answer
+    $('[data-carnap-type=translate]',
+      '[data-carnap-type=qualitative]',
+      '[data-carnap-type=proofchecker]').each(function () {
       const exerciseId = $(this).attr('data-carnap-submission');
-      const studentWork = $(this).find('input').val();
+      const studentWork = $(this).find('input', 'textarea').val();
       as['Saved Work'][exerciseId] = studentWork;
     });
 
     // Truth Tables
+    $('[data-carnap-type=truthtable]').each(function () {
+      const exerciseId = $(this).attr('data-carnap-submission');
+      as['Saved Work'][exerciseId] = [];
+      $(this).find('select').each(function () {
+        as['Saved Work'][exerciseId].push($(this).val());
+      });
+    });
     // Derivations
+    $('[data-carnap-type=proofchecker]').each(function () {
+      const exerciseId = $(this).attr('data-carnap-submission');
+      const studentWork = $(this).find('textarea').val();
+      as['Saved Work'][exerciseId] = studentWork;
+    });
     // Model Checking
-    // Qualitative Problems
+    // Multiple Choice and Numerical
     // Sequent Calculus Problems
     // Gentzen-Prawitz Natural Deduction Problems
 
-    console.log(as);
+    console.log(JSON.stringify(as));
 
     // If we can't putArgumentState, that probably means we aren't
     // a Carnap.io assignment.
@@ -50,21 +64,38 @@ function initSaveWork() {
   }
 
   function loadWork() {
-    // Syntax Checking
-    // Translation
-    $('[data-carnap-type=translate]').each(function () {
-      // Use data-carnap-submission as unique identifier
-      // This will break more than one problem with same label on page
+    // Syntax Checking (not implemented)
+    //
+    // Translation and Qualitative Short Answer
+    $('[data-carnap-type=translate]',
+      '[data-carnap-type=qualitative]').each(function () {
       const exerciseId = $(this).attr('data-carnap-submission');
-      const studentWork = as['Saved Work'][exerciseId];
-
-      $(this).find('input').val(studentWork);
+      if (typeof as['Saved Work'][exerciseId] !== 'undefined') {
+        const studentWork = as['Saved Work'][exerciseId];
+        $(this).find('input', 'textarea').val(studentWork);
+      }
     });
 
     // Truth Tables
+    $('[data-carnap-type=truthtable]').each(function () {
+      const exerciseId = $(this).attr('data-carnap-submission');
+      if (typeof as['Saved Work'][exerciseId] !== 'undefined') {
+        $(this).find('select').each(function () {
+          const value = as['Saved Work'][exerciseId].shift();
+          $(this).val(value);
+        });
+      }
+    });
     // Derivations
+    $('[data-carnap-type=proofchecker]').each(function () {
+      const exerciseId = $(this).attr('data-carnap-submission');
+      if (typeof as['Saved Work'][exerciseId] !== 'undefined') {
+        const studentWork = as['Saved Work'][exerciseId];
+        $(this).find('textarea').val(studentWork);
+      }
+    });
     // Model Checking
-    // Qualitative Problems
+    // Multiple Choice and Numerical
     // Sequent Calculus Problems
     // Gentzen-Prawitz Natural Deduction Problems
 
@@ -74,6 +105,8 @@ function initSaveWork() {
 
   loadWork();
   $(window).on('beforeunload', saveWork);
+  $(window).on('blur', saveWork);
 }
 
-$(document).ready(initSaveWork);
+// $(document).ready(initSaveWork);
+$(window).on('load', initSaveWork);
