9cd5a13b3a5587414bf5388c05eeda1f86c325f3
David Sanson <dsanson@gmail.com>
2020-07-26 11:56:25 -0500

    let putAssignmentState deal with stringifying. Should fix #1

--- js/checklist.js
+++ js/checklist.js
@@ -66,9 +66,9 @@ function checklist() {
         console.log(as);
 
         // If we can't putArgumentState, that probably means we aren't
-        // a Carnap.io assignment.
+        // running within a Carnap assignment.
         try {
-          putAssignmentState(JSON.stringify(as));
+          putAssignmentState(as);
         } catch {
           console.log('Unable to putArgumentState');
         }
